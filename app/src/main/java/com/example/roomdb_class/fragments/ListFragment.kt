077a5621bb82bd1a6faa723004cfd06b405f86bb
Eduardo <edu_felip@hotmail.com>
2021-07-12 04:15:52 -0300

    add view binding and destroy users option

--- app/src/main/java/com/example/roomdb_class/fragments/ListFragment.kt
+++ app/src/main/java/com/example/roomdb_class/fragments/ListFragment.kt
@@ -1,26 +1,80 @@
 package com.example.roomdb_class.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdb_class.R
-import kotlinx.android.synthetic.main.fragment_list.view.*
+import com.example.roomdb_class.adapters.ListAdapter
+import com.example.roomdb_class.viewmodels.UserViewModel
+import com.example.roomdb_class.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mUserViewModel: UserViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
 
-        view.fab.setOnClickListener {
+        //RecyclerView
+        val adapter = ListAdapter()
+        val recyclerView = binding.listRecycler
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //UserViewModel
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.listUsers.observe(viewLifecycleOwner, { users ->
+            adapter.setUserList(users)
+        })
+
+        binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.item_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete) {
+            deleteAllUsers()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAllUsers() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mUserViewModel.deleteAllUsers()
+            Toast.makeText(requireContext(), "All Users Successfully Deleted", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No") { _, _ ->
+
+        }
+        builder.setTitle("Delete All Users?")
+        builder.setMessage("Are you sure about that?")
+        builder.create().show()
     }
 }
